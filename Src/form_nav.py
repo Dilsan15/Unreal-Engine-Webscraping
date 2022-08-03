13f37125f93f557b1a53a2cbfffa6e0de8d873d7
Dilsan15 <dilshaan.sandhu6@gmail.com>
2022-08-02 18:28:11 -0600

    Initial Commit

---
+++ Src/form_nav.py
@@ -0,0 +1,154 @@
+import time
+from datetime import datetime
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+class FormNavigator:
+
+    def __init__(self, basic_link, website_page, driver_path, time_out, broswer_vis):
+
+        self.basic_link = basic_link
+        self.website_page = website_page
+        self.current_link = basic_link + website_page
+
+        self.driver_path = driver_path
+        self.time_out = time_out
+
+        self.link_stored = list()
+
+        self.b_soup = None
+
+        sel_service = Service(self.driver_path)
+
+        option = webdriver.ChromeOptions()
+        option.add_argument('--incognito')
+
+        if not broswer_vis:
+            option.add_argument("--headless")
+
+        self.driver = webdriver.Chrome(service=sel_service, options=option)
+        self.driver.get(f'{self.current_link}')
+
+    def scroll_page(self, user_request="down", num_of_times="infinite"):
+
+        time.sleep(self.time_out)
+        at_bottom = None
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+        if num_of_times == "infinite":
+
+            while True:
+
+                if user_request == "up":
+                    self.driver.execute_script("window.scrollTo(0,0)")
+                elif user_request == "down":
+                    new_height = self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+                time.sleep(self.time_out)
+
+                new_height = self.driver.execute_script("return document.body.scrollHeight")
+
+                if last_height == new_height:
+                    at_bottom = True
+                    break
+
+                last_height = new_height
+
+        else:
+
+            count = 0
+
+            while count != int(num_of_times):
+                self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(self.time_out)
+                new_height = self.driver.execute_script("return document.body.scrollHeight")
+                count += 1
+
+            if last_height == new_height:
+                at_bottom = True
+
+            else:
+                last_height = new_height
+
+        return at_bottom
+
+    def refresh_page(self):
+        self.driver.refresh()
+
+    def get_page_html(self):
+
+        self.b_soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+        return self.b_soup, self.basic_link, self.website_page
+
+    def get_page_meta(self):
+
+        time_now = datetime.now()
+        self.b_soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+
+        try:
+
+            info_u_list = self.b_soup.find("div", class_="topic-map")
+
+            date_list = [date.get("title") for date in info_u_list.find_all("span", class_="relative-date") if
+                         date.get("title") is not None][0:2]
+
+            li_numbers = [num.text if num.text is not None and not (num.text[-1] == "k") else num.get("title")
+                          for num in info_u_list.find_all("span", class_="number")]
+
+            des_text = [decs.text for decs in info_u_list.find_all("h4") if decs.text is not None]
+            meta_data_dict = {des_text[string]: (date_list + li_numbers)[string] for string in range(len(des_text))}
+
+            if "likes" not in meta_data_dict and "like" not in meta_data_dict: meta_data_dict["likes"] = "0"
+            if "links" not in meta_data_dict and "link" not in meta_data_dict: meta_data_dict["links"] = "0"
+
+            meta_data_dict["date-scraped"] = time_now.strftime(f'%b %d, %Y %I:%M:%S %p')
+            meta_data_dict["title"] = self.b_soup.find("a", class_="fancy-title").text.strip()
+
+            return meta_data_dict
+
+        except AttributeError:
+
+            meta_data_dict = {'title': f'{self.b_soup.find("a", class_="fancy-title").text.strip()}',
+                              'date-scraped': f"{time_now.strftime('%b %d, %Y %I:%M:%S %p')}",
+                              'created': f'{self.b_soup.find("span", class_="relative-date").get("title")}',
+                              'last reply': 'No-Date', 'replies': '0', 'views': 'Unknown', 'users': '1',
+                              'likes': 'Unknown',
+                              'links': 'Unknown'}
+
+            return meta_data_dict
+
+    def get_link(self):
+
+        for a_tag in self.b_soup.find_all("a"):
+
+            try:
+                a_tag = (a_tag.get("href")).rsplit('/', 1)[0]
+
+                if a_tag.startswith("/t/") and a_tag.count("/") == 2:
+                    a_tag += "/"
+                    self.link_stored.append(a_tag)
+
+            except AttributeError as a:
+                continue
+
+        self.link_stored = list(dict.fromkeys(self.link_stored))
+
+    def get_num_of_link(self):
+        return len(self.link_stored)
+
+    def remove_link(self, num):
+
+        for nu in range(0, num):
+            self.link_stored.pop()
+
+    def set_link(self, user_request):
+
+        self.website_page = self.link_stored[0]
+        self.current_link = self.basic_link + self.website_page
+        self.driver.get(self.current_link)
+
+        self.link_stored.pop(0)
+
+        return self.current_link, user_request
---
+++ Src/word_scraper.py
@@ -0,0 +1,85 @@
+import csv
+import re
+from bs4 import BeautifulSoup
+
+
+class DiscourseWordScraper:
+
+    def __init__(self, basic_link, website_page, time_zone, specific_classes):
+
+        self.specific_classes = specific_classes
+        self.basic_link = basic_link
+        self.website_page = website_page
+
+        self.time_zone = time_zone
+
+        self.b_Soup = None
+
+    def set_var(self, html_data, website_page):
+        self.b_Soup = BeautifulSoup(html_data.strip(), 'html.parser')
+        self.website_page = website_page
+
+    def get_by_class(self):
+
+        temp_data = list()
+
+        for class_txt in self.specific_classes:
+            temp_data += self.b_Soup.find_all(class_=class_txt)
+
+        return ([re.split('[,.\s/]', x.text) for x in temp_data], self.specific_classes)
+
+    def validate_input(self, temp_data, type_cases=None, stats_dict=None):
+
+        black_list = ["http", "img", "<p>"]  # <- NEEDS INPUT TO RUN
+        regex = re.compile("[^a-zA-Z-/_']")
+        final_input = list()
+
+        for raw_sentence in temp_data:
+            raw_input = ([regex.sub('', string.lower()) for string in raw_sentence])
+            final_input.extend([string for string in raw_input if
+                                any(blackTag in string for blackTag in black_list) is False and string != ""])
+
+        if stats_dict is not None and type_cases is not None:
+            final_input.insert(0, stats_dict["title"])
+            final_input.insert(1, stats_dict["date-scraped"] + " MDT")
+            final_input.insert(2, (self.basic_link + self.website_page))
+            final_input.insert(3, stats_dict["created"] + " MDT")
+            final_input.insert(4, stats_dict["last reply"] + " MDT")
+
+            try:
+                final_input.insert(5, stats_dict["replies"])
+            except AttributeError:
+                final_input.insert(5, stats_dict["reply"])
+
+            try:
+                final_input.insert(6, stats_dict["views"])
+            except AttributeError:
+                final_input.insert(6, stats_dict["view"])
+
+            try:
+                final_input.insert(7, stats_dict["users"])
+            except AttributeError:
+                final_input.insert(7, stats_dict["user"])
+
+            try:
+                final_input.insert(8, stats_dict["likes"])
+            except AttributeError:
+                final_input.insert(8, stats_dict["like"])
+
+            try:
+                final_input.insert(9, stats_dict["links"])
+            except AttributeError:
+                final_input.insert(9, stats_dict["link"])
+
+            final_input.insert(10, type_cases)
+
+        return final_input
+
+    def save_CSV(self, final_input):
+
+        with open('Data_Collected/data_collected.csv', 'r', encoding="utf-8") as dfr, open(
+                'Data_Collected/data_collected.csv', 'a+', newline='',encoding="utf-8") as dfw:
+
+            writer = csv.writer(dfw, delimiter=',')
+            reader = csv.reader(dfr)
+            writer.writerow(final_input)
